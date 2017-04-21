606d82c34fa351bde3038e6a33a36a31b3f2e305
Facundo Olano <facundo.olano@gmail.com>
2017-04-21 13:19:08 -0300

    use new clj-http version to allow form-params in DELETE

--- project.clj
+++ project.clj
@@ -4,6 +4,6 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
-                 [clj-http "3.4.1"]
+                 [clj-http "3.5.0"]
                  [venantius/ultra "0.5.1"]
                  [cheshire "5.7.0"]])
